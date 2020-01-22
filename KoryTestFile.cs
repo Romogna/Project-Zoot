8df90bf9d078d0a392ddb68d9f87053e9343b990
korymenke <kmenke1@jhu.edu>
2020-01-21 21:04:24 -0600

    Added a sample program to the repository for GitHub practice

---
+++ KoryTestFile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		int Hex = 6;
+		int Oct = 8;
+		int Square = 4;
+		int line = 2;
+		int Tri = 3;
+		
+		Tri++;
+
+
+		int equation = Tri*Hex;
+        Console.WriteLine(equation);
+
+	}
+}
