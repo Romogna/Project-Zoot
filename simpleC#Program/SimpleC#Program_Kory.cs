55a8710b86951ff6a1a755753aa8b323675484ab
korymenke <kmenke1@jhu.edu>
2020-02-04 21:30:16 -0600

    Added a simple C# Program

---
+++ simpleC#Program/SimpleC#Program_Kory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        int firstNumber = 5;
+        int secondNumber = 10;
+        firstNumber += secondNumber;
+        Console.WriteLine(firstNumber); //this adds the second number to the first and sets that to the value of the first
+        secondNumber *= firstNumber;
+        Console.WriteLine(secondNumber); //this multiplies the first and second number and assigns that value to the second number
+        Console.WriteLine("Emery loves Little Ceasers Pizza");
+    }
+}
