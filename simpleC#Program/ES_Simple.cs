34b814f107f8ddedbf1abe400fea78b825ec2d60
catshirtguy <emland2@outlook.com>
2020-02-11 13:53:32 -0800

    Add files via upload

---
+++ simpleC#Program/ES_Simple.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ES_simple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int moo = 5;
+            string kory = "is a cows name";
+
+            if (moo > 4)
+            {
+                Console.WriteLine("Kory is a Cows name!!!");
+            }
+            else Console.WriteLine("Kory is defentily a Turtle");
+        }
+    }
+}
+
