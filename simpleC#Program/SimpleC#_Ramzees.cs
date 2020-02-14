4cdd0be891b6c8466359028a038175826cb6bcba
Badroads <55846139+Badroads@users.noreply.github.com>
2020-02-14 13:45:48 -0700

    Added C# script

---
+++ simpleC#Program/SimpleC#_Ramzees.cs
@@ -0,0 +1,13 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int x = 20;
+	    int y = 45;
+		int z = x*y;
+		Console.WriteLine(z);
+		Console.WriteLine("Hello World");
+	}
+}
