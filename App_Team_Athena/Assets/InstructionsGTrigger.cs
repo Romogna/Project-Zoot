631975bcaf5bed3e6263879d86435de35f1f1c9c
catshirtguy <emland2@outlook.com>
2020-02-24 00:00:52 -0800

    Began Geology App UI

---
+++ App_Team_Athena/Assets/InstructionsGTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstructionsGTrigger : MonoBehaviour
+{
+    public InstructionsG dialogue
+        ;
+
+    public void TriggerInstructions()
+    {
+        FindObjectOfType<InstructionsGeologyManager>().StartInstructions(dialogue);
+    }
+}
---
+++ App_Team_Athena/Assets/InstructionsGeologyManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InstructionsGeologyManager : MonoBehaviour
+{
+    public Text nameText;
+    public Text dialogueText;
+
+    private Queue<string> instructions;
+
+    void Start()
+    {
+        instructions = new Queue<string>();
+    }
+
+    public void StartInstructions(InstructionsG dialogue)
+    {
+        Debug.Log("Start " + dialogue.name);
+ //       nameText.text = dialogue.name;
+
+        instructions.Clear();
+
+        foreach (string instruction in dialogue.instructions)
+        {
+            instructions.Enqueue(instruction);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (instructions.Count == 0)
+        {
+        EndDialogue();
+        return;
+        }
+
+        string instruction = instructions.Dequeue();
+        //       Debug.Log(instruction);
+        dialogueText.text = instruction;
+    }
+
+    void EndDialogue()
+    {
+        Debug.Log("End of Conversation");
+    }
+}
