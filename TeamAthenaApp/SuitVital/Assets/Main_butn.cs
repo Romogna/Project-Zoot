78bca0ef849be1539ec41e56913aabcce056fe82
rmhowe425 <45905457+rmhowe425@users.noreply.github.com>
2020-05-07 18:49:19 -0400

    Add files via upload

--- TeamAthenaApp/SuitVital/Assets/Main_butn.cs
+++ TeamAthenaApp/SuitVital/Assets/Main_butn.cs
@@ -1,4 +1,4 @@
-﻿//voice from Initial Navigation App
+﻿//voice from Initial Navigation App form other apps
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,28 +6,84 @@ using UnityEngine.UI;
 // Voice
 using UnityEngine.Windows.Speech;
 using System.Linq;
+using UnityEngine.Networking;
+
 
 public class Main_butn : MonoBehaviour
 {
     public GameObject pan;
-    //public GameObject warn_pan;
-    //public GameObject e_pan;
+    public GameObject warn_pan;
+    public GameObject e_pan;
     public GameObject super_but;
     public Text btext;
     public Text b1text;
+    Teleinfo info;
+    public Text counttext;
     //Text txt = transform.Find("Text").GetComponent<Text>();
     // Speech
     KeywordRecognizer keywordRecognizer = null;
     Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
     float timeLeft = 30.0f;
-    //int a = 0;
+   
+
+
+    //display text in the panel
+    //[SerializeField] Text counttext;
+
+    [System.Serializable]
+    public class Teleinfo
+    {
+        public string _id;
+        public int time;
+        public string timer;
+        public string started_at;
+        public int heart_bpm;
+        //2-4 psid
+        public string p_sub;
+        //2-4 psid
+        public string p_suit;
+        //standard
+        public string t_sub;
+        //10000-40000 rpm
+        public string v_fan;
+        //750-950
+        public string p_o2;
+        //.5-1
+        public string rate_o2;
+        
+        public double batteryPercent;
+        public int battery_out;
+        //0-30
+        public int cap_battery;
+        //0-10 hrs
+        public string t_battery;
+        //14-16
+        public string p_h2o_g;
+        //14-16
+        public string p_h2o_l;
+        //750-950
+        public string p_sop;
+        //0.5-1
+        public string rate_sop;
+        public string t_oxygenPrimary;
+        public string t_oxygenSec;
+        public string ox_primary;	
+        public string ox_secondary;
+        //0-10
+        public string t_oxygen;
+        public double cap_water;
+        //0-10 hrs
+        public string t_water;
+        public int __v;
+    }
+
     // Use this for using keywords to activate functions
     void Start()
     {
-
-
-
         //b1text.text = "Sign";
+        //connect to database
+        //StartCoroutine(GetRequest("http://localhost:3000/api/simulation/state"));
+
         // Global Voice Command
         keywords.Add("mini", () =>
         {
@@ -57,31 +113,37 @@ public class Main_butn : MonoBehaviour
     }
     void Update()
     {
+        //StartCoroutine(GetRequest("http://localhost:3000/api/simulation/state"));
+        //60-100
+
 
         if (timeLeft >= -4)
         {
             timeLeft -= Time.deltaTime;
             Debug.Log(timeLeft);
-            if (timeLeft < 15)
+            if (timeLeft < 24)
             {
                 Debug.Log("warning");
-                //warn_pan.SetActive(true);
+                warn_pan.SetActive(true);
                 super_but.GetComponent<Image>().color = Color.yellow;
 
 
+
+
             }
-            if (timeLeft < 5)
+            if (timeLeft < 10)
             {
                 Debug.Log("emergency");
-                //warn_pan.SetActive(false);
-                //e_pan.SetActive(true);
+
                 super_but.GetComponent<Image>().color = Color.red;
+                warn_pan.SetActive(false);
+                e_pan.SetActive(true);
             }
-            if (timeLeft < -3)
-            {
-                //e_pan.SetActive(false);
-                super_but.GetComponent<Image>().color = Color.green;
-            }
+           
+        }else {
+            e_pan.SetActive(false);
+            warn_pan.SetActive(false);
+
         }
 
     }
@@ -100,12 +162,7 @@ public class Main_butn : MonoBehaviour
         {
             txt.text = "Mini";
             pan.SetActive(true);
-
-
         }
-
-
-
     }
 
     public void Display()
@@ -129,6 +186,51 @@ public class Main_butn : MonoBehaviour
         if (keywords.TryGetValue(args.text, out keywordAction))
         {
             keywordAction.Invoke();
+        }
+    }
+
+
+
+    IEnumerator GetRequest(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            string[] pages = uri.Split('/');
+            int page = pages.Length - 1;
+
+            if (webRequest.isNetworkError)
+            {
+                Debug.Log(pages[page] + ": Error: " + webRequest.error);
+                counttext.text =  webRequest.error;
+            }
+            else
+            {
+                //Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+                counttext.text = webRequest.downloadHandler.text;
+                //JSONObject json = new JSONObject(www.text);
+                string response = System.Text.Encoding.UTF8.GetString(webRequest.downloadHandler.data);
+                //Teleinfo player= JsonUtility.ToJson(response);
+                //Teleinfo player = JsonConvert.DeserializeObject<Teleinfo>(response);
+                // Teleinfo t = new Teleinfo();
+                //Teleinfo json = new Teleinfo(webRequest.downloadHandler.text);
+                //Teleinfo 
+                info =JsonUtility.FromJson<Teleinfo>(webRequest.downloadHandler.text);
+                Debug.Log(info.timer.ToString());
+                /*if (info.heart_bpm < 60 || info.heart_bpm > 100)
+                {
+
+                    Debug.Log("Emergency");
+
+                    super_but.GetComponent<Image>().color = Color.red;
+                    warn_pan.SetActive(false);
+                    e_pan.SetActive(true);
+
+
+                }*/
+            }
         }
     }
 }
