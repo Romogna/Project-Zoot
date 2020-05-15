cc2e610fa2aebe7b8dc780f6d63bbb18a13f9d21
choug3055@connect.durhamtech.edu <choug3055@connect.durhamtech.edu>
2020-05-15 16:25:36 -0400

    QR_no_thread

---
+++ TeamAthenaApp/QR_test/Assets/BarcodeCam.cs
@@ -0,0 +1,201 @@
+﻿// Threading
+using System.Threading.Tasks;
+// Aux
+using System.Collections.Generic;
+//Speech
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+// QR Reader
+using ZXing;
+using ZXing.QrCode;
+
+
+public class BarcodeCam : MonoBehaviour
+{
+    // Texture for encoding test
+    public Texture2D encoded;
+
+    private WebCamTexture camTexture;
+
+    private Color32[] cam;
+    private int W, H;
+
+    private Rect screenRect;
+
+    private bool isQuit;
+
+    public string LastResult;
+
+    private bool shouldEncodeNow;
+
+    // Speech
+    KeywordRecognizer keywordRecognizer = null;
+    Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+
+    //Task qrThread = new Task(Print());
+
+    void OnGUI()
+    {
+        GUI.DrawTexture(screenRect, camTexture, ScaleMode.ScaleToFit);
+    }
+
+    void OnEnable()
+    {
+        if (camTexture != null)
+        {
+            camTexture.Play();
+            W = camTexture.width;
+            H = camTexture.height;
+        }
+    }
+    void OnDisable()
+    {
+        if (camTexture != null)
+        {
+            camTexture.Pause();
+        }
+    }
+
+    void OnDestroy()
+    {
+        //qrThread.Sleep();
+        camTexture.Stop();
+    }
+    /*
+    // It's better to stop the thread by itself rather than abort it.
+    void OnApplicationQuit()
+    {
+        isQuit = true;
+    }
+    */
+    void Start()
+    {
+        //Keywords();
+        encoded = new Texture2D(256, 256);
+        LastResult = "{Placeholder}";
+        shouldEncodeNow = true;
+
+        screenRect = new Rect(0, 0, Screen.width, Screen.height);
+
+        camTexture = new WebCamTexture();
+        camTexture.requestedHeight = Screen.height; // 480;
+        camTexture.requestedWidth = Screen.width; //640;
+        OnEnable(); // Use voice control
+
+        //Task.Run(() => Print());
+        Task.Run(() => DecodeQR());         
+    }
+
+    void Update()
+    {
+        if (cam == null)
+        {
+            cam = camTexture.GetPixels32();
+        }
+
+        // encode the last found
+        var textForEncoding = LastResult;
+        if (shouldEncodeNow &&
+            textForEncoding != null)
+        {
+            var color32 = Encode(textForEncoding, encoded.width, encoded.height);
+            encoded.SetPixels32(color32);
+            encoded.Apply();
+            shouldEncodeNow = false;
+        }
+    }
+    /*
+    async Task Print()
+    {
+        Debug.Log("Working?");
+        Task.Delay(1000).Wait();
+        await Print();
+    }
+    */
+    private Task DecodeQR()
+    {
+        // create a reader with a custom luminance source
+        var barcodeReader = new BarcodeReader { AutoRotate = false, TryHarder = false };
+        Debug.Log("Decoding");
+
+        while (true)
+        {
+
+            try
+            {
+                // decode the current frame
+                var result = barcodeReader.Decode(cam, W, H);
+                Debug.Log("Trying");
+                if (result != null)
+                {
+                    Debug.Log("Result not Null");
+                    LastResult = result.Text;
+                    shouldEncodeNow = true;
+                    return Task.Run(() => { Debug.Log(result.Text); });
+                }
+
+                // Sleep a little bit and set the signal to get the next frame
+                //Thread.Sleep(200);
+                Task.Delay(1000).Wait();
+                cam = null;
+            }
+            catch
+            {
+                Debug.Log("Catch");
+                Task.Delay(1000).Wait();
+                return Task.Run(() => { Debug.Log("failed"); });
+            }
+        }
+    }
+    
+    private static Color32[] Encode(string textForEncoding, int width, int height)
+    {
+        var writer = new BarcodeWriter
+        {
+            Format = BarcodeFormat.QR_CODE,
+            Options = new QrCodeEncodingOptions
+            {
+                Height = height,
+                Width = width
+            }
+        };
+        return writer.Write(textForEncoding);
+    }
+
+    // Keyword library
+    void Keywords()
+    {
+        // Global Voice Command
+        keywords.Add("Camera Enable", () =>
+        {
+            // Call the changeTarget function
+            OnEnable();
+        });
+        keywords.Add("Camera Disable", () =>
+        {
+            // Call the changeTarget function
+            OnDisable();
+        });
+        keywords.Add("Minimize Info", () =>
+        {
+            //toggleArrow();
+        });
+
+        // Tell the KeywordRecognizer about our keywords.
+        keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+
+        // Register a callback for the KeywordRecognizer and start recognizing!
+        keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+        keywordRecognizer.Start();
+    }
+    // Voice commands function
+    private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        System.Action keywordAction;
+        if (keywords.TryGetValue(args.text, out keywordAction))
+        {
+            keywordAction.Invoke();
+        }
+    }
+}
---
+++ TeamAthenaApp/QR_test/Assets/BarcodeGenerator.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using ZXing;
+using ZXing.QrCode;
+using ZXing.Common;
+using System.Threading;
+[RequireComponent(typeof(RawImage))]
+
+public class BarcodeGenerator : MonoBehaviour
+{
+    [SerializeField] private BarcodeFormat format = BarcodeFormat.QR_CODE;
+    [SerializeField] private string data = "test";
+    [SerializeField] private int width = 512;
+    [SerializeField] private int height = 512;
+
+    public RawImage cRawImage;
+
+    private void Start()
+    {
+        //cRawImage = GetComponent();
+        cRawImage = GetComponent<RawImage>();
+
+        // Generate the texture
+        Texture2D tex = GenerateBarcode(data, format, width, height);
+
+        // Setup the RawImage
+        cRawImage.texture = tex;
+        cRawImage.rectTransform.sizeDelta = new Vector2(tex.width, tex.height);
+    }
+
+    private Texture2D GenerateBarcode(string data, BarcodeFormat format, int width, int height)
+    {
+        // Generate the BitMatrix
+        BitMatrix bitMatrix = new MultiFormatWriter().encode(data, format, width, height);
+
+        // Generate the pixel array
+        Color[] pixels = new Color[bitMatrix.Width * bitMatrix.Height];
+        int pos = 0;
+        for (int y = 0; y < bitMatrix.Height; y++)
+        {
+            for (int x = 0; x < bitMatrix.Width; x++)
+            {
+                pixels[pos++] = bitMatrix[x, y] ? Color.black : Color.white;
+            }
+        }
+
+        // Setup the texture
+        Texture2D tex = new Texture2D(bitMatrix.Width, bitMatrix.Height);
+        tex.SetPixels(pixels);
+        tex.Apply();
+        return tex;
+    }
+
+ /**
+    public Result decode(Uri uri)
+    {
+        Bitmap image;
+        try
+        {
+            image = (Bitmap)Bitmap.FromFile(uri.LocalPath);
+        }
+        catch (Exception)
+        {
+            throw new FileNotFoundException("Resource not found: " + uri);
+        }
+
+        using (image)
+        {
+            LuminanceSource source;
+            source = new BitmapLuminanceSource(image);
+            BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+            Result result = new MultiFormatReader().decode(bitmap);
+            if (result != null)
+            {
+                //... code found
+            }
+            else
+            {
+                //... no code found
+            }
+            return result;
+        }
+    }
+**/
+}
